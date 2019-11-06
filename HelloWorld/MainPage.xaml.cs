bac9cb1c428b98f46963e12ef28efe84e31c8ec7
sunbreak <sunbreak.wang@gmail.com>
2019-11-06 21:13:39 +0800

    Add `Hello` `World` buttons
    Signed-off-by: sunbreak <sunbreak.wang@gmail.com>

--- HelloWorld/MainPage.xaml.cs
+++ HelloWorld/MainPage.xaml.cs
@@ -1,17 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+﻿using System.Diagnostics;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -25,6 +14,16 @@ namespace HelloWorld
         public MainPage()
         {
             this.InitializeComponent();
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            Debug.WriteLine("Hello");
+        }
+
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+            Debug.WriteLine("World");
         }
     }
 }
