91b84e931a758d75efb7e017c06147580a87e7e1
sunbreak <sunbreak.wang@gmail.com>
2019-11-12 05:22:18 +0800

    Add HelloLibrary project
    Signed-off-by: sunbreak <sunbreak.wang@gmail.com>

---
+++ HelloLibrary/HelloClass.cs
@@ -0,0 +1,17 @@
+﻿using System.Diagnostics;
+
+namespace HelloLibrary
+{
+    public class HelloClass
+    {
+        public static void Hello()
+        {
+            Debug.WriteLine("Hello");
+        }
+
+        public static void World()
+        {
+            Debug.WriteLine("World");
+        }
+    }
+}
--- HelloWorld/MainPage.xaml.cs
+++ HelloWorld/MainPage.xaml.cs
@@ -1,29 +1,29 @@
-﻿using System.Diagnostics;
-using Windows.UI.Xaml;
+﻿using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using HelloLibrary;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
 namespace HelloWorld
 {
     /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
+    ///     An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
     public sealed partial class MainPage : Page
     {
         public MainPage()
         {
-            this.InitializeComponent();
+            InitializeComponent();
         }
 
         private void button1_Click(object sender, RoutedEventArgs e)
         {
-            Debug.WriteLine("Hello");
+            HelloClass.Hello();
         }
 
         private void button2_Click(object sender, RoutedEventArgs e)
         {
-            Debug.WriteLine("World");
+            HelloClass.World();
         }
     }
 }
